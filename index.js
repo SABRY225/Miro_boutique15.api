c963499791178b676e217555672e4e4afc69dc2e
SABRY225 <ahmed010144497@gmail.com>
2025-01-30 02:03:23 +0200

    5 commit

--- index.js
+++ index.js
@@ -60,4 +60,6 @@ if (!MONGO_URI) {
 }
 
 // تصدير التطبيق بدون `listen()`
-module.exports = app;
+// module.exports = app;
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
